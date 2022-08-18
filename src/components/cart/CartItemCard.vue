892706e6169027baf29434ad595295cf99dc5601
Kindred <KDSM@protonmail.com>
2022-08-18 18:21:35 +0100

    responsiveness update

--- src/components/cart/CartItemCard.vue
+++ src/components/cart/CartItemCard.vue
@@ -25,13 +25,13 @@ export default {
 
 <style lang="scss">
 .cart-item-card {
-  width: 90%;
+  margin: 0 auto;
   margin: 5%;
   background-color: white;
   box-shadow: 0 0 5px gray;
   border-radius: 5px;
   padding: 10px;
-  text-align: left;
+  text-align: center;
 }
 
 .header {
